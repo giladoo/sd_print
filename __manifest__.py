7a58d738772cfc9dcda621cb4f5b4d7f770049d6
arash <homayounfar.arash@gmail.com>
2025-01-16 22:23:47 +0330

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Report Direct Print',
+    'version': '15.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Directly print reports from the browser',
+    'description': """
+        This module allows users to print reports directly from the browser and display the print dialog.
+    """,
+    'website': 'https://giladoo.com',
+    'author': 'Arash Homayounfar',
+    'depends': ['base', 'web',],
+    'data': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'sd_print/static/src/js/action_service.js',
+        ],
+    },
+
+    'installable': True,
+    'application': False,
+}
